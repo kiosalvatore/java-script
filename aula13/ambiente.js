05cc0851dd44ef2746a1926112f7c8996931cf58
kiosilva <cariocacaiogomes@gmail.com>
2023-11-09 23:01:04 -0300

    commit complemento dowhile

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -2,7 +2,7 @@ var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
-} while (c <= 6)
+ }  while (c <= 6)
 
 /*
 var c = 1 
--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -3,3 +3,20 @@ for(var c = 1; c <= 6; c++ ) {
     console.log(c)
 }
 console.log('FIM!')
+
+/* Escrevendo o exemplo acima de outra maneira ficaria:
+
+            var c = 1 
+                while c <=10{
+                    console.log('Isso é menor do que 10')
+                    c++
+} 
+
+Ou:
+
+            var c = 1
+                do {
+                    console.log('Isso é menor do que 10')
+                } while (c <= 10)
+
+*/
