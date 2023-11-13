cf97685b94dc0f302696c9f7434e3e2b909c3ac9
kiosilva <cariocacaiogomes@gmail.com>
2023-11-12 21:06:31 -0300

    commit reforço array

--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -13,7 +13,7 @@ console.log(valores)
 console.log(valores[1])
 
 
-for(pos=0; pos <= valores.length; pos++) {
+for(pos=0; pos < valores.length; pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
 
@@ -24,3 +24,23 @@ for(pos=0; pos <= valores.length; pos++) {
 for(var pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
+
+/* Exemplo indexOf() */
+
+var valores = [8, 4, 2, 5, 7, 9]
+console.log(valores)
+var pos = valores.indexOf(8)
+console.log(`O valor 8 está na posição  ${pos}`)
+
+/* Ou podemos escrever assim */
+
+var valores = [8, 4, 2, 5, 7, 9]
+console.log(valores)
+var pos = valores.indexOf(8)
+
+if (pos == -1) {
+    window.alert('O valor não foi encontrado')
+} else {
+    console.log(`O valor 8 está na posição  ${pos}`)
+}
+
